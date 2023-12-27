64754434b8cbd07798c6d477ef8720b004919e9f
Timothy Tandt <Timothy.tandt@outlook.be>
2023-12-27 17:37:14 +0100

    Initial Install
    Installed the Next 14 app
    Installed ShadCN UI

---
+++ hydrovoltz/app/(chats)/page.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
+const ChatPage = () => {
+  return (
+    <div className="p-8 flex w-full h-full">
+      <ModeToggle />
+    </div>
+  );
+};
+
+export default ChatPage;
