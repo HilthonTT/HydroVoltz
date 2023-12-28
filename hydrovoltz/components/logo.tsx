fc1b300c52830b80f2665ad53c7c25c4cbead31f
Timothy Tandt <Timothy.tandt@outlook.be>
2023-12-28 15:11:58 +0100

    [DB & Webhook]: Added the Clerk Webhook and prisma db schema
    - Made the User table
    - Added the webhook responsible for creating, updating and deleting a user

--- hydrovoltz/components/logo.tsx
+++ hydrovoltz/components/logo.tsx
@@ -16,7 +16,7 @@ export const Logo = ({ className }: LogoProps) => {
           "h-28 w-28 relative bg-white rounded-full border-2 border-gray-500 dark:border-0",
           className
         )}>
-        <Image src="./logo.svg" alt="Logo" className="object-cover" fill />
+        <Image src="/logo.svg" alt="Logo" className="object-cover" fill />
       </div>
       <div className="hidden lg:block text-center">
         <p className="text-2xl font-semibold">HydroVoltz</p>
